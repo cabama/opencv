9a021107278a5c96b58b51fd756ff6971245196b
cabama <barreymata@gmail.com>
2015-10-14 16:00:21 +0200

    Comentada openCamera

--- practica1/openCamera.py
+++ practica1/openCamera.py
@@ -1,20 +1,26 @@
+# Importamos las librerias necesarias
 import numpy as np
 import cv2
 
+# Abrimos la webcam y asignamos este objeto a una variable.
 cap = cv2.VideoCapture(0)
 
+# Entramos en un  bucle
 while(True):
-    # Capture frame-by-frame
+
+    # capturamos el frame actual
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
+    # Convertimos este frame a escala de grises
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
+    # Mostramos el frame en escala de grises
     cv2.imshow('frame',gray)
+
+    # Si se pulsa la tecla 'Q' se saldra del bucle
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
+# Tras haber salido del bucle, liberamos la camara y cerramos todas las ventanas.
 cap.release()
 cv2.destroyAllWindows()
